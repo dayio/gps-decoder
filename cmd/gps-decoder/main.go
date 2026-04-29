e4417a5d826173ed901c81b0d6cff690b767a389
Alexandre Mangin <amangin@gorilla-ltd.com>
2026-04-29 09:56:16 +0200

    feat(decoder): read binary IQ

--- cmd/gps-decoder/main.go
+++ cmd/gps-decoder/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Project initialized")
+	file, err := os.Open("./data/gpssim.bin")
+
+	if err != nil {
+		log.Fatalf("Error : %v", err)
+	}
+
+	buffer := make([]int8, 4000)
+
+	err = binary.Read(file, binary.LittleEndian, &buffer)
+
+	if err != nil {
+		log.Fatalf("Error : %v", err)
+	}
+
+	for i := 0; i < 10; i += 2 {
+		signalI := buffer[i]
+		signalQ := buffer[i+1]
+		fmt.Printf("sample %d -> I: %4d, Q: %4d\n", (i/2)+1, signalI, signalQ)
+	}
 }
