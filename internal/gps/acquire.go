d1cff310f10dea7dc97113641e0ec34965a42ed6
Alexandre Mangin <amangin@gorilla-ltd.com>
2026-05-09 15:07:37 +0200

    feat(gps): implement core GPS acquisition function using Gold Code correlation

---
+++ internal/gps/acquire.go
@@ -0,0 +1,50 @@
+package gps
+
+import (
+	"math/cmplx"
+)
+
+func Acquire(samples []complex128, prn int, sampleRate float64) (int, float64) {
+	goldCode := GenerateGoldCode(prn)
+
+	maxPower := 0.0 // Peak signak
+	sumPower := 0.0 // Used to calculate the SNR
+	count := 0      // Number of "samples"
+
+	bestPhase := 0
+
+	// We slide the window across the samples
+	for phase := range samples {
+
+		var correlation complex128
+
+		// We check our 1023-bit key against this specific window
+		for i := range 1023 {
+
+			// Calculate the signal index with the offset (modulo to wrap around)
+			// This also maps the 1023 chips to the 2000 samples
+			sigIdx := (phase + int(float64(i)*(sampleRate/1023000.0))) % len(samples)
+
+			// Simple correlation, Signal * Code (Without Doppler compensation)
+			correlation += samples[sigIdx] * complex(goldCode[i], 0)
+		}
+
+		power := cmplx.Abs(correlation)
+
+		// We add this power to the total noise baseline
+		sumPower += power
+		count++
+
+		// Check if we find a new peak
+		if power > maxPower {
+			maxPower = power
+			bestPhase = phase
+		}
+	}
+
+	// Calculate the average noise and the SNR
+	avgNoise := sumPower / float64(count)
+	snr := maxPower / avgNoise
+
+	return bestPhase, snr
+}
